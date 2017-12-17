5ade12e10156df40132394b3da2c6678663c487f
Aaron Wilder <element1028@gmail.com>
2017-12-16 22:30:31 -0800

    some ui changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,45 @@
 import React, { Component } from 'react';
 import Clock from './Clock';
 import './App.css';
+import { Form, FormControl, Button } from 'react-bootstrap';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            deadline: 'December 25, 2018',
-            newDeadline: ''
+            deadline: 'May 4, 2018',
+            newDeadline: '',
+            goal: 'Countdown to Graduation'
         }
     }
     changeDeadline() {
         this.setState({deadline: this.state.newDeadline});
     }
+
     render() {
         return (
             <div className="App">
                 <div className="App-title">
-                    Countdown to {this.state.deadline}
+                    {this.state.goal} <br />{this.state.deadline}
                 </div> 
                 <Clock 
                     deadline={this.state.deadline}
                 />
-                <div>
-                    <input 
+                <Form inline>
+                    <FormControl
+                    className="Deadline-input"
                         placeholder='new date'
                         onChange={event => this.setState({newDeadline: event.target.value})}
+                        onKeyPress={event => {
+                            if (event.key === 'Enter') {
+                                this.changeDeadline()
+                            }
+                        }}
                     />
-                    <button onClick={() => this.changeDeadline()}>
+                    <Button onClick={() => this.changeDeadline()}>
                         Submit
-                    </button>
-                </div>   
+                    </Button>
+                </Form>   
             </div>
         )
     }
